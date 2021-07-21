10d1d642b090af63de204e92190dbdf26afa9281
APilganchuk <opilganchuk@gmail.com>
2021-07-21 18:29:02 +0300

    lesson 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { Component } from "react";
 import { Panel } from "./components/Panel/Panel";
-import ProductList from "./components/Products/Products";
+import Counter from "./components/Counter";
+import Products from "./components/Products";
+import ColorPicker from "./components/ColorPicker";
 import products from "./products.json";
+import colorPickerOptions from "./colorPickerOptions.js";
+import Dropdown from "./components/Dropdown";
+import TodoList from "./components/TodoList";
 import "./App.css";
 
 const color = {
@@ -8,16 +14,51 @@ const color = {
   second: "red",
 };
 const { prime, second } = color;
-const App = () => {
-  return (
-    <div>
-      <Panel color={prime} />
-      <Panel color={second} />
 
-      <ProductList products={products}>
-        <h3 style={{ color: "tomato" }}>PRODUCTS</h3>
-      </ProductList>
-    </div>
-  );
-};
+const BASCET = {};
+products.forEach((product) => (BASCET[product.name] = 0));
+
+class App extends Component {
+  state = {
+    todos: [],
+    search: "",
+    BASCET,
+  };
+
+  handleChange = (e) => {
+    this.setState({ search: e.target.value });
+  };
+  updateBascet = (productId, newValue) => {
+    this.setState({ BASCET: { ...BASCET, [productId]: newValue } });
+  };
+  render() {
+    const { search, BASCET } = this.state;
+
+    const filteredProducts = products.filter((product) =>
+      product.name.toLowerCase().includes(search.toLowerCase())
+    );
+    let bascetMessage = "";
+    return (
+      <div>
+        <TodoList todos={this.state.todos} />
+        <ColorPicker options={colorPickerOptions} />
+        <Counter initialValue={0} />
+        <Dropdown />
+
+        <Panel color={prime} />
+        <Panel color={second} />
+
+        <Products
+          bascet={BASCET}
+          updateBascet={this.updateBascet}
+          handleChange={this.handleChange}
+          products={filteredProducts}
+        >
+          <h3 style={{ color: "tomato" }}>PRODUCTS</h3>
+          <p>Bascet:{bascetMessage}</p>
+        </Products>
+      </div>
+    );
+  }
+}
 export default App;

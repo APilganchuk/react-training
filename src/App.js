eac0870f2419428b913aa0e056d2a06703aac229
APilganchuk <opilganchuk@gmail.com>
2021-07-28 15:23:35 +0300

    l5Repeta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
 import Form from "./components/Form/Form";
 import TodoEditor from "./components/TodoEditor";
-
+import Modal from "./components/Modal";
 class App extends Component {
   state = {
     todos: "",
+    showModal: false,
   };
   addTodo = (text) => {
     const todo = { text };
@@ -17,10 +18,25 @@ class App extends Component {
     }, 2000);
   };
 
+  toggleModal = () => {
+    this.setState((prewState) => ({ showModal: !prewState.showModal }));
+  };
+
   render() {
-    
     return (
       <div>
+        <button onClick={this.toggleModal} type="button">
+          Open
+        </button>
+        {this.state.showModal && (
+          <Modal onClose={this.toggleModal}>
+            <p>hello</p>
+            <button onClick={this.toggleModal} type="button">
+              Close
+            </button>
+          </Modal>
+        )}
+
         <Form onSubmit={this.formSubmitHandler} />
         <TodoEditor onSubmit={this.addTodo} />
       </div>
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import styles from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+const modalRoot = document.querySelector("#modal-root");
+
+class Modal extends Component {
+  componentDidMount() {
+    console.log("Modal componentDidMount");
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    console.log("Modal componentWillUnmount");
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown = (e) => {
+    if (e.code === "Escape") {
+      this.props.onClose();
+    }
+  };
+  handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return createPortal(
+      <div
+        className={styles.Modal__backdrop}
+        onClick={this.handleBackdropClick}
+      >
+        <div className={styles.Modal__content}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+export default Modal;

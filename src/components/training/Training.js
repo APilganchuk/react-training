193820ca14c03dbf435f97a99dbdfc232b4d3fff
APilganchuk <opilganchuk@gmail.com>
2021-08-19 17:37:46 +0300

    done

--- src/components/training/Training.js
+++ src/components/training/Training.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useLocation, useHistory } from "react-router";
 
-export default function Training() {
+export default function Training({ title }) {
+  const location = useLocation();
+  const history = useHistory();
+
+  const onCLickBack = () => {
+    history.push(location.state?.goBack ?? "/books");
+  };
+
   return (
     <div>
-      <h1>training</h1>
+      <h1>{title}</h1>
+
+      <button onClick={onCLickBack}>
+        {location.state?.label ?? "go back"}
+      </button>
     </div>
   );
 }
--- src/components/views/HomeView.js
+++ src/components/views/HomeView.js
@@ -1,16 +1,41 @@
 import { NavLink } from "react-router-dom";
 import { Route } from "react-router";
+import { useLocation } from "react-router";
 import Training from "../training/Training";
-import Training2 from "../training/Training2";
 
 export default function HomeView() {
+  const location = useLocation();
   return (
     <div className="wrapper">
-      <NavLink to="/smolin">Redux_Smolin</NavLink>
-      <NavLink to="/repeta">Redux_Repeta</NavLink>
+      <NavLink
+        to={{
+          pathname: "/smolin",
+          state: {
+            goBack: location,
+            label: "go home",
+          },
+        }}
+      >
+        Redux_Smolin
+      </NavLink>
+      <NavLink
+        to={{
+          pathname: "/repeta",
+          state: {
+            goBack: location,
+            label: "go home",
+          },
+        }}
+      >
+        Redux_Repeta
+      </NavLink>
 
-      <Route path="/smolin" component={Training} />
-      <Route path="/repeta" component={Training2} />
+      <Route path="/smolin">
+        <Training title="one" />
+      </Route>
+      <Route path="/repeta">
+        <Training title="two" />
+      </Route>
     </div>
   );
 }
